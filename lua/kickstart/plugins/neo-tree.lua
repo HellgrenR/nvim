ea99c504eeddabe569498fcd56b221eee5ca0788
HellgrenR <rasmus.hellgren1@ingka.ikea.com>
2025-09-02 22:54:15 +0200

    fix: neo-tree now sees hidden

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,12 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- This is what makes hidden files visible
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
